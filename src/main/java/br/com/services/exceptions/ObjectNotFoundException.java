d117a7477d633731d6c8640e445d69023b88618b
alessandroscoelho <alesandroscoelho@gmail.com>
2024-01-01 13:35:44 -0300

    demais commits

---
+++ src/main/java/br/com/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/br/com/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/services/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.com.domain.User;
 import br.com.repositories.UserRepository;
 import br.com.services.UserService;
+import br.com.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
 	public User findById(Integer id) {
 		Optional<User> obj = userRepository.findById(id);
 
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuario nao encontrado."));
 	}
 
 }
