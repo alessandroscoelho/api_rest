8760fb41ac9d3b3f6e32d44f53d98cc91999076b
alessandroscoelho <alesandroscoelho@gmail.com>
2024-01-01 14:20:39 -0300

    commit modelMapper

---
+++ src/main/java/br/com/domain/dto/UserDTO.java
@@ -0,0 +1,74 @@
+package br.com.domain.dto;
+
+import java.util.Objects;
+
+public class UserDTO {
+
+	private Integer id;
+	private String name;
+	private String email;
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserDTO(Integer id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public UserDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- src/main/java/br/com/resources/UserResource.java
+++ src/main/java/br/com/resources/UserResource.java
@@ -6,20 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.config.ModelMapperConfig;
 import br.com.domain.User;
+import br.com.domain.dto.UserDTO;
 import br.com.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+
+	@Autowired
+	private ModelMapperConfig mapper;
+	
 	@Autowired
 	private UserService userService;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id) {
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
-		return ResponseEntity.ok().body(userService.findById(id));
+		return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
 	}
 
 }
